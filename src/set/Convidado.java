d054e4d73223fbab42db2206dfca5f9b05f1e291
brunnocky <brunno-cardoso-santana@outlook.com>
2023-09-27 14:06:24 -0300

    Commit collections

---
+++ src/set/Convidado.java
@@ -0,0 +1,42 @@
+package set;
+
+import java.util.Objects;
+
+public class Convidado {
+    private String nome;
+    private int codigo;
+
+    public Convidado(String nome, int codigo) {
+        this.nome = nome;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Convidado convidado = (Convidado) o;
+        return codigo == convidado.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", codigo=" + codigo +
+                '}';
+    }
+}
